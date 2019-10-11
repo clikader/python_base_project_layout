936aa8e8b4d145e98589d75615923754a01ff022
Li <clikader@gmail.com>
2019-10-11 15:20:54 -0500

    init push

---
+++ README.md
@@ -0,0 +1,1 @@
+#### Put something you want to say here
---
+++ Settings.py
@@ -0,0 +1,13 @@
+import os
+
+# User's Settings
+
+# Program Settings
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Database Settings
+DB_HOSTNAME = ''
+DB_DATABASE = ''
+DB_USERNAME = ''
+DB_PASSWORD = ''
+DB_PORT = ''
